fd03fa34d9397beefaea95fe66e9436dd1c89597
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-02-27 00:18:53 -0800

    Se agrega una prueba para verificar si años divisibles entre 100 pero no por 400 no son años bisiestos

--- src/aniobisiesto.spec.js
+++ src/aniobisiesto.spec.js
@@ -4,4 +4,7 @@ describe("Determinar si es un año bisiesto", () => {
     it("Si un año es divisible por 400 se retorna true", () => {
         expect(aniobisiesto(2000)).toEqual(true);
     });
+    it("Si un año es divisible por 100 pero no por 400 se retorna false", () => {
+        expect(aniobisiesto(1900)).toEqual(false);
+    });
 });
