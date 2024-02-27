3934a1d3765abdbc8699bb9311656eb8baa3b06b
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-02-27 03:17:28 -0800

    Se agrega una funcionalidad que permite dividir entre 4 y retornar como verdadero un año bisiesto

--- src/aniobisiesto.js
+++ src/aniobisiesto.js
@@ -2,6 +2,9 @@ function aniobisiesto(anio){
     if(anio%400 == 0){
         return true;
     }
+    else if(anio%4 == 0 && anio%100!=0){
+        return true;
+    }
     return false;
 }
 
--- src/aniobisiesto.spec.js
+++ src/aniobisiesto.spec.js
@@ -7,4 +7,7 @@ describe("Determinar si es un año bisiesto", () => {
     it("Si un año es divisible por 100 pero no por 400 se retorna false", () => {
         expect(aniobisiesto(1900)).toEqual(false);
     });
+    it("Si un año es divisible por 4 pero no por 100 se retorna true", () => {
+        expect(aniobisiesto(2016)).toEqual(true);
+    });
 });
